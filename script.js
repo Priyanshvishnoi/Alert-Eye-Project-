eed35ebdd1eb9225c441aafc17df1392562f211c
Priyansh Vishnoi <priyanshvishnoi592@gmail.com>
2025-08-08 00:06:41 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const video = document.getElementById('video');
+
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then(stream => {
+    video.srcObject = stream;
+  })
+  .catch(err => {
+    alert("Camera access denied or unavailable.");
+    console.error(err);
+  });
